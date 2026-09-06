56a145b31499ac36d9a892838977d9ce9014b324
Sudais Salisu <sudaissalisuu@gmail.com>
2026-09-06 06:04:11 +0000

    fix: add onLogout prop to NavUser component

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -31,12 +31,14 @@ import {
 
 export function NavUser({
   user,
+  onLogout,
 }: {
   user: {
     name: string
     email: string
     avatar: string
   }
+  onLogout?: () => void
 }) {
   const { isMobile } = useSidebar()
 
@@ -98,7 +100,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={onLogout}>
               <IconLogout />
               Log out
             </DropdownMenuItem>
